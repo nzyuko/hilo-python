47168724d7a3d2cce3b1098951b1ba55c7c75866
nzyuko <johnnzyuko514@gmail.com>
2022-04-03 21:26:34 +0300

    calculate range for integer input

--- hilo.py
+++ hilo.py
@@ -1,6 +1,22 @@
 import random
 
 
+def user_range():
+    try:
+        # calculate range for integer input
+        floor = int(input("Input the floor of your range(Minimum guessable number: )"))
+        ceiling = int(input("Input the ceiling of your range(Maximum guessable number: )"))
+        return ceiling - floor
+    except ValueError:
+        try:
+            # convert it to a float
+            number = float(input("Enter your number and hit Enter: "))
+            return number
+        except ValueError:
+            # recall function again
+            print("Please input a number")
+            userinput()
+
 def userinput():
     try:
         # convert input to integer
@@ -16,3 +32,4 @@ def userinput():
             print("Please input a number")
             userinput()
 
+def pick_random():
